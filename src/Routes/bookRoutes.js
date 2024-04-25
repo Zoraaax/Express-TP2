bd370d529f055405df5096a15789f8a93505c929
Valentin Thibault <valentin.thlt@gmail.com>
2024-04-25 14:16:44 +0200

    chore: change variable name

--- src/Routes/bookRoutes.js
+++ src/Routes/bookRoutes.js
@@ -3,9 +3,13 @@ import { bookController } from "../Controllers/bookController.js";
 export const bookRoutes = (app) => {
   const bookControllerInstance = new bookController();
 
-  app.route("/livres").get(controller.getBooks.bind(controller));
-  app.route("/livres/:id").get(controller.getBookById.bind(controller));
-  app.route("/ajout-livre").post(controller.createBook.bind(controller));
+  app.route("/livres").get(bookControllerInstance.getBooks.bind(controller));
+  app
+    .route("/livres/:id")
+    .get(bookControllerInstance.getBookById.bind(controller));
+  app
+    .route("/ajout-livre")
+    .post(bookControllerInstance.createBook.bind(controller));
   app
     .route("/livres/auteur/:auteur")
     .get(controller.getBookByAuthor.bind(controller));
